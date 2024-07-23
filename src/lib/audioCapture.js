69a0c203d947691e76b377fb22d520e635bd5152
salvation <salvationibeh@gmail.com>
2024-07-23 08:32:42 +0100

    Writing audioCapture.js

---
+++ src/lib/audioCapture.js
@@ -0,0 +1,40 @@
+// src/lib/audioCapture.js
+
+class AudioCapture {
+    constructor(options = {}) {
+      this.options = {
+        sampleRate: options.sampleRate || 44100,
+        bitDepth: options.bitDepth || 16,
+        enableNoiseReduction: options.enableNoiseReduction !== false,
+        enableEchoCancellation: options.enableEchoCancellation !== false,
+      };
+      this.micStream = null;
+      this.tabStream = null;
+      this.isCapturing = false;
+      this.isPaused = false;
+      this.audioContext = null;
+      this.micSource = null;
+      this.tabSource = null;
+      this.processor = null;
+      this.analyser = null;
+      this.audioLevelCallback = null;
+      this.onErrorCallback = null;
+      this.capturedChunks = [];
+    }
+  
+    async start() {
+      if (this.isCapturing) return;
+  
+      try {
+        await this.requestPermissions();
+        await this.setupAudioContext();
+        await this.captureMicAudio();
+        await this.captureTabAudio();
+        this.setupAudioProcessing();
+        this.isCapturing = true;
+        this.startVisualization();
+      } catch (error) {
+        this.handleError(error);
+      }
+    }
+}
