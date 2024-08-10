b050e085d3ab4795883305136f1929cd08e05e90
salvation <salvationibeh@gmail.com>
2024-08-10 14:02:30 +0100

    updated gitignore(remove config.js)

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    TRANSLATION_API_KEY: 'your_translation_api_key',
+    SPEECH_TO_TEXT_API_KEY: 'your_speech_to_text_api_key',
+    TEXT_TO_SPEECH_API_KEY: 'your_text_to_speech_api_key'
+  };
